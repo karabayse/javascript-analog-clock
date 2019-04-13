b4b617d7a357552cc22e5b10d68bc251db517a68
Kara Bayse <karabayse@gmail.com>
2019-04-13 16:18:49 -0500

    added hour position

--- client.js
+++ client.js
@@ -5,3 +5,14 @@ const SECONDHAND = document.querySelector('#second');
 let hrPosition = 20;
 let minPosition = 130;
 let secPosition = 267;
+
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + "Minute: " + min + "Second: " + sec);
+
+let hrPosition = (hr*360/12) + (min*(360/60)/12);
+let minPosition =
+let secPosition =
